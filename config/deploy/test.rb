c01dfe25fb36c84303d3f308ac860ff117dcad13
Ed Madrid <edmadrid@gmail.com>
2017-09-26 16:01:15 -0400

    updates cap cul gemfile and fixes urls in deploy configs

--- config/deploy/test.rb
+++ config/deploy/test.rb
@@ -1,4 +1,4 @@
 server "#{fetch(:department)}-nginx-#{fetch(:stage)}1.cul.columbia.edu", user: fetch(:remote_user), roles: %w{web app}
 ask :branch, proc { `git tag --sort=version:refname`.split("\n").last }
 
-set :url, 'http://educatingharlem.cdrs.columbia.edu/review'
+set :url, 'http://educatingharlem-test.cul.columbia.edu'
